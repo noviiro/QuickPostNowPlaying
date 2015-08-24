02cbba2cfa6dcab7c418450b662a8d120f863228
noviiro <nobu@noviiro.net>
2015-08-24 20:54:27 +0900

    Initial Commit

---
+++ QuickPostNowPlaying/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickPostNowPlaying
+//
+//  Created by Nobuhiro Ito on 2015/08/24.
+//  Copyright (c) 2015年 noviiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
